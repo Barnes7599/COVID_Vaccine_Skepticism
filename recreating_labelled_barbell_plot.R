308de1146f5e01a72b351356aa42dcf52d4c6ece
Jason Barnes <43148197+Barnes7599@users.noreply.github.com>
2022-02-19 08:56:47 -0500

    custom theme

--- recreating_labelled_barbell_plot.R
+++ recreating_labelled_barbell_plot.R
@@ -529,7 +529,135 @@ data %>%
         panel.grid = element_blank(),
         panel.background = element_blank(),
         plot.background = element_blank()
+    ) +
+    theme(
+        text = element_text(family = "lato"),
+        plot.title = element_textbox_simple(family = "patua-one",
+                                            size = 33,
+                                            lineheight = 1,
+                                            margin = margin(b=10)), 
+        plot.caption =  element_markdown(lineheight = 1.3,
+                                         margin = margin(t=10),
+                                         hjust = 0,
+                                         color = "#666666",
+                                         family = "lato"),
+        plot.subtitle = element_text(family = "lato",
+                                     size = 18),
+        axis.title.y = element_text(color = "#888888",
+                                    size = 12),
+        axis.text.y = element_text(color = "#888888", 
+                                   size = 12),
+        axis.text.x = element_text(color = "#888888",
+                                   size = 12),
+        axis.line = element_blank(),
+        plot.title.position = "plot",
+        plot.caption.position = "plot",
+        panel.grid = element_blank(),
+        panel.background = element_blank(),
+        plot.background = element_blank()
     )
 
 
 
+
+# Custom Theme darkmode()----
+
+
+
+p <- data %>% 
+    mutate(country = fct_reorder(country,-october),
+           change = case_when(august < october ~ "increasing",
+                              august > october ~ "decreasing",
+                              TRUE ~ "stable")) %>% 
+    
+    pivot_longer(cols = c("august","october"), 
+                 names_to = "month", 
+                 values_to = "percent") %>% 
+    mutate(month = factor(month, 
+                          levels = c("august", "october"),
+                          labels = c("August '20","October '20"))) %>%
+    
+    ggplot(aes(x = month, 
+               y = percent, 
+               group = country,
+               color = change)) +
+    geom_line(show.legend = FALSE, 
+              size = .7) +
+    
+    geom_text_repel(data = country_labels, 
+                    aes(x = month, 
+                        y= percent, 
+                        label = country,  
+                        color = change),
+                    size = 5, 
+                    # inherit.aes = FALSE,
+                    hjust = "outward",
+                    # for geom_label_repel
+                    # label.padding = unit(1.1, "mm"),
+                    # label.size = unit(.5,"mm"),
+                    # label.r = unit(.2,"mm"),
+                    family = "lato",
+                    min.segment.length = 0.5,
+                    nudge_x = country_labels$nudge_x,
+                    # nudge_y = country_labels$nudge, 
+                    show.legend = FALSE) +
+    scale_y_continuous(breaks = seq(50, 100, 10),
+                       lim = c(50,100)) +
+    scale_color_manual(values = c("#9e1201", "#b3b3b3", "#b3b3b3")) +
+    labs(
+        title = "COVID-19 Vaccination Intent is <span style='color:#B51300'>Decreasing</span> Globally",
+        subtitle = "From August 2020 to October 2020",
+        caption = "<i>Base: 18,526 online aduts aged 16-74 across 15 countries</i><br>Source: Ipsos",
+        x = NULL,
+        y = "Percent willing to receive vaccine",
+        tag = NULL,
+        color = NULL
+    ) 
+
+
+theme_darkmode <- function(bg = "#212121", 
+                           primary = "#ffffff", 
+                           secondary = "##B51300"){
+    
+    theme_gray() %+replace%
+        
+    theme(
+        text = element_text(family = "lato"),
+        plot.title = element_textbox_simple(family = "patua-one",
+                                            size = 33,
+                                            lineheight = 1,
+                                            margin = margin(b=10),
+                                            color = primary
+                                            ),
+        plot.caption =  element_markdown(lineheight = 1.3,
+                                         margin = margin(t=10),
+                                         hjust = 0,
+                                         color = primary,
+                                         family = "lato"),
+        plot.subtitle = element_text(family = "lato",
+                                     size = 18,
+                                     color = primary),
+        axis.title.y = element_text(color = primary,
+                                    size = 12,
+                                    angle = 90,
+                                    margin = margin(r=10)),
+        axis.text.y = element_text(color = primary,
+                                   size = 12),
+        axis.text.x = element_text(color = primary,
+                                   size = 12),
+        axis.line.y = element_blank(),
+        axis.ticks = element_blank(),
+        plot.title.position = "plot",
+        plot.caption.position = "plot",
+        panel.grid = element_blank(),
+        panel.background = element_rect(fill = bg,
+                                        color = NA),
+        plot.background = element_rect(fill = bg,
+                                       color = NA)
+        
+    )
+}
+
+    
+p + 
+    theme_darkmode()
